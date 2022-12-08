64132bdfafa207d5c30294c60bfde21c1fa26767
Doug <douglas.rice@salesforce.com>
2022-12-08 14:06:11 -0500

    feature-1: Adding additional console log

--- default/lwc/gitCommitDemo/gitCommitDemo.js
+++ default/lwc/gitCommitDemo/gitCommitDemo.js
@@ -4,5 +4,6 @@ export default class GitCommitDemo extends LightningElement {
 
     connectedCallback() {
         console.log('Please commit me?');
+        console.log('Pretty please?');
     }
 }
