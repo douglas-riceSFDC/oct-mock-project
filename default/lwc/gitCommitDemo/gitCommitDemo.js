2f660e6318b64324fb88c7d65807516f844054cb
Doug <douglas.rice@salesforce.com>
2022-12-08 14:04:32 -0500

    feature-1: Adding gitCommitDemo

---
+++ default/lwc/gitCommitDemo/gitCommitDemo.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class GitCommitDemo extends LightningElement {
+
+    connectedCallback() {
+        console.log('Please commit me?');
+    }
+}
